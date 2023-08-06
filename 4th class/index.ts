eadb2d57a3ef7fa00151537c886bb06111137183
Mehrkhan <139866560+Mehrkhan@users.noreply.github.com>
2023-08-06 12:14:45 +0500

    1st task

---
+++ 4th class/index.ts
@@ -0,0 +1,18 @@
+
+//for(let i=0;i<5;i++){
+//console.log("Hello gandu" + i);
+//}
+
+//let arr=["pesh","lahore","rawp"]
+
+//loops
+
+//for(initialization,condition,iteration)
+
+// let arry=["Usman","Zia","Wania"],temp;
+
+let arr1=[1,3,-4,7,8,-9,11,12,-14,13,16,15,17,-18,20]
+
+for(let even=0;even%2;even++){
+    console.log()
+}
